505c0f75690d97f0ad81b66831a4b7b8156306c3
alvesantos <alvesantos.gh@gmail.com>
2026-08-23 15:00:17 -0300

    feat: add migration files for creating and dropping users table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('driver', 'customer', 'admin')),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    document VARCHAR(11) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
